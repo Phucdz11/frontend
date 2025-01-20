6b657327c88d546e342c423bd56e7c12b5ae97d5
Phucdz11 <136161215+Phucdz11@users.noreply.github.com>
2025-01-20 22:47:27 +0700

    Update Register.js

--- user/src/components/Register.js
+++ user/src/components/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
         } else if (fullname.length === 0) {
             alert("Full name has been left blank!");
         } else {
-            const url = 'https://frontend-vku8.vercel.app/api/register' ; 
+            const url = 'https://frontend-vku8.vercel.app/api/user/register' ; 
             const userData = {
                 username,
                 password,
